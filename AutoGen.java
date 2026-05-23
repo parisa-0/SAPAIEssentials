38cea91d7c30e4dab318e3fc5ae4f44cc70888e2
Parisa Noushazaran <134794618+parisa-0@users.noreply.github.com>
2026-05-23 13:35:15 +0100

    Create AutoGen.java

---
+++ AutoGen.java
@@ -0,0 +1,12 @@
+
+--------------------------------------------------------------------------------
+assistant (to user_proxy):
+
+***** Suggested tool call (call_1q7fB92eMq498LhMo6hKx3Ld): fetch_product_data_tool *****
+Arguments: 
+{"product_id": 3}
+****************************************************************************************
+***** Suggested tool call (call_KeqWeGP593Rm8qbWZ4Doqdp5): convert_currency_tool *****
+Arguments: 
+{"amount_usd": 10.0, "target_currency": "GBP"}
+**************************************************************************************
